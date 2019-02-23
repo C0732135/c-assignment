ca73bec47e5ea5fa617686c0bd3ab980e7755a45
visualstudio <visualstudio@122-PC03>
2019-02-22 20:53:14 -0500

    8.54 commit

--- Assihnment 1/Program.cs
+++ Assihnment 1/Program.cs
@@ -6,15 +6,6 @@ using System.Threading.Tasks;
 using System.IO;
 
 
-namespace Assihnment_1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        { }
-
-    }
-}
 class JournalEntry
 {
     public JournalEntry(string note, int dist)
@@ -116,18 +107,16 @@ class CountrySide
         this.Announcement();
     }
 
-    public void Announcement()
+  public void Announcement()
     {
         try
         {
-            // Create an instance of StreamReader to read from a file.
-            // The using statement also closes the StreamReader.
-            using (StreamReader sr = new StreamReader("c:/area51/annoucement.txt"))
+           
+            using (StreamReader sr = new StreamReader("s:/users/C099999/annoucement.txt"))
             {
                 string line;
 
-                // Read and display lines from the file until 
-                // the end of the file is reached. 
+                
                 while ((line = sr.ReadLine()) != null)
                 {
                     Console.WriteLine(line);
@@ -136,8 +125,8 @@ class CountrySide
         }
         catch (Exception e)
         {
-            // Let the user know what went wrong.
-            Console.WriteLine("The file could not be read:");
+           
+           Console.WriteLine("The file could not be read:");
             Console.WriteLine(e.Message);
         }
     }
